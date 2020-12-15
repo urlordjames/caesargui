0f8e4e5105cabdd5b59c2f32de12aa477910d601
urlordjames <urlordjames@gmail.com>
2020-12-14 20:46:31 -0500

    made text input multiline

--- src/window.cpp
+++ src/window.cpp
@@ -6,7 +6,7 @@ wxEND_EVENT_TABLE()
 
 fMain::fMain(): wxFrame(nullptr, wxID_ANY, "ceasargui") {
   column = new wxBoxSizer(wxVERTICAL);
-  text = new wxTextCtrl(this, wxID_ANY, "lorem ipsum", wxDefaultPosition, wxSize(100, 30));
+  text = new wxTextCtrl(this, wxID_ANY, "lorem ipsum", wxDefaultPosition, wxSize(100, 30), wxTE_MULTILINE);
   offset = new wxSpinCtrl(this, wxID_ANY, "offset", wxDefaultPosition, wxSize(50, 50), wxSP_ARROW_KEYS, 0, 25, 3, "dog");
   button = new wxButton(this, 1, "encrypt", wxDefaultPosition, wxSize(100, 30));
 
